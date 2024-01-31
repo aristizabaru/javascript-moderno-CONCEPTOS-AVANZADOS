0926683d1fa5a6f8ae55d6b74eb93fb2d6ae3ce7
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-01-31 06:43:27 -0500

    feat: implement example of Promise.all()

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -9,6 +9,10 @@ export const promiseComponent = (element) => {
         element.innerHTML = hero.name
     }
 
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `${hero1.name} / ${hero2.name}`
+    }
+
     const renderError = (error) => {
         element.innerHTML = `<span style="color:red">Error:</span> ${error}`
     }
@@ -19,6 +23,40 @@ export const promiseComponent = (element) => {
         // .then(hero => renderHero(hero)) -- si los parámetros son iguales se puede mandar ref de la función
         .then(renderHero)
         .catch(renderError)
+
+
+    //! FORMAS DE MANEJAR VARIAS PROMESAS
+    const id1 = '5d86371fd55e2e2a30fe1ccb'
+    const id2 = '5d86371fd55e2e2a30fe1ccb1'
+
+    //! Forma 1 (RECOMENDADA - Si los valores no son dependientes)
+
+    Promise.all([findHero(id1), findHero(id2)])
+        .then(([hero1, hero2]) => renderTwoHeroes(hero1, hero2))
+        .catch(renderError)
+
+    //! Forma 2 (no recomendada - Retornar promesa)
+    // let hero1
+
+    // findHero(id1)
+    //     .then(hero => {
+    //         hero1 = hero
+    //         return findHero(id2)
+    //     }).then(hero2 => {
+    //         renderTwoHeroes(hero1, hero2)
+    //     })
+    //     .catch(renderError)
+
+    //! Forma 3 (no recomendada - Promise hell)
+    // findHero(id1)
+    //     .then(hero1 => {
+    //         findHero(id2)
+    //             .then(hero2 => {
+    //                 renderTwoHeroes(hero1, hero2)
+    //             })
+    //             .catch(renderError)
+    //     })
+    //     .catch(renderError)
 }
 
 /**
