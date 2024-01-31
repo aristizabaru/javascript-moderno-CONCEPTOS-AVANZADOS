8b302b37a153cc0067c29469f8523e76d6b8c7f6
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-01-31 05:44:47 -0500

    feat: error handle with callbacks

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -5,19 +5,32 @@ import { heroes } from "../data/heroes"
  * @param {HTMLDivElement} element 
  */
 export const callbackComponent = (element) => {
-    const id = '5d86371f2343e37870b91ef1'
-    findHero(id, ({ name }) => {
-        element.innerHTML = `The hero's name is ${name}`
+    const id = '5d86371f2343e37870b91ef12'
+    findHero(id, (error, hero) => {
+        // -- optional chaning (?.) & nullish coalescing operator (??)
+        // element.innerHTML = hero?.name ?? 'No hay heroe'
+        if (error) {
+            element.innerHTML = error
+            return
+        }
+
+        element.innerHTML = hero.name
     })
 }
 
 /**
  * 
  * @param {String} id 
- * @param {(hero:Object)=>void} callback 
+ * @param {(error: String | null, hero:Object)=>void} callback 
  */
 const findHero = (id, callback) => {
     const hero = heroes.find(hero => hero.id === id)
 
-    callback(hero)
+    // El estandar de la industria es mandar el error como primer argumento del callbak
+    if (!hero) {
+        callback(`Hero with id ${id} not found`)
+        return
+    }
+
+    callback(null, hero)
 }
