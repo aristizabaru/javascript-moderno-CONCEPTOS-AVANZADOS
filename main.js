c9ca8b89c82f8a6ea7fb209b8f68e62e9172a598
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-02 15:18:12 -0500

    feat: add async implementation

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { enviromentComponent } from './src/concepts/01-enviroment'
 import { callbackComponent } from './src/concepts/02-callbacks'
 import { promiseComponent } from './src/concepts/03-promises'
 import { promiseRaceComponent } from './src/concepts/04-promise-race'
+import { asyncComponent } from './src/concepts/05-async'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -23,5 +24,6 @@ const element = document.querySelector('.card')
 // enviromentComponent(element)
 // callbackComponent(element)
 // promiseComponent(element)
-promiseRaceComponent(element)
+// promiseRaceComponent(element)
+asyncComponent(element)
 
---
+++ src/concepts/05-async.js
@@ -0,0 +1,27 @@
+import { heroes } from "../data/heroes"
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncComponent = (element) => {
+
+    const id1 = '5d86371fd55e2e2a30fe1ccbs'
+
+    findHero(id1)
+        .then(name => element.innerText = name)
+        .catch(error => element.innerText = error)
+}
+
+
+/**
+ * 
+ * @param {String} id 
+ */
+const findHero = async (id) => {
+
+    const hero = heroes.find(hero => hero.id === id)
+    if (!hero) throw `Hero with id ${id} not found`
+
+    return hero.name
+}
